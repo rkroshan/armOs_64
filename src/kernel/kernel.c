7d746e70f85674a9e4f0bfe23bfee09a2db8a894
rkroshan <rkroshan.1999@gmail.com>
2023-07-01 22:08:35 +0530

    Feature: Qemu Raspi3b: add format printk support - supports %x %u %d %s

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,10 +1,16 @@
 #include "uart/uart.h"
+#include "utils/print.h"
 
 /*uart testing*/
 void uart_test(void)
 {
     init_uart();
     write_string("Welcome! Uart is working \nHola!!\n");
+    printk("Using Printk Function !!\r\n");
+    printk("Testing Hex val: %x\n",0x0123456789ABCDEF);
+    printk("Testing unsigned val: %u\n",2);
+    printk("Testing signed val: %d\n",-4); /*%d will only support int32*/
+    printk("Testing string : %s\n","this is pretty cool\n");
 }
 
 void kernel_main()
